1bc17cf1bb2f04c6511cbeb7708a9014963bd067
Null-Root <triascend94@gmail.com>
2022-08-27 20:16:21 +0800

    tsft finished

---
+++ FileHandler.cpp
@@ -0,0 +1,430 @@
+<<<<<<< HEAD
+#include "FileHandler.h"
+
+void FileHandler::PrintHelp()
+{
+	std::cout
+		<< "    Create New File:                    tsft +fl <name>							\n"
+		<< "    Create New Files:                   tsft +fl \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Create New Folder:                  tsft +fd <name>							\n"
+		<< "    Create New Folders:                 tsft +fd \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Show Stuffs:                        tsft -p <depth:opt>		                \n"
+		<< "        Show All with Filter (included optional depth after -p)								\n"
+		<< "\n"
+		<< "        Substring Filters:              tsft -p <depth:opt> -sf <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -sf \"<name_1>, <name_2>, ...\"	\n"
+		<< "        Category Filters:               tsft -p <depth:opt> -cf <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -cf \"<name_1>, <name_2>, ...\"	\n"
+		<< "        Extension Filters:              tsft -p <depth:opt> -ef <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -ef \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Clone Path:                         tsft -c <depth:opt> <dest> <src:opt>				\n"
+		<< "        without passing depth explicitly, depth will be set to 0							\n"
+		<< "\n"
+		<< "        Clone with Substring													            \n"
+		<< "        Substring Filters:              tsft -c <depth> -sf \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "        Category Filters:               tsft -c <depth> -cf \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "        Extension Filters:              tsft -c <depth> -ef \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "\n\n"
+		<< "    Remove Existing File:               tsft -fl <name>							\n"
+		<< "    Remove Existing Files:              tsft -fl \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Remove Existing Folder:             tsft -fd <name>							\n"
+		<< "    Remove Existing Folders:            tsft -fd \"<name_1>, <name_2>, ...\"	\n"
+		<< std::endl;
+}
+
+FileHandler::FileHandler(Config config)
+{
+	this->_config = config;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFile(std::string srcPath, int depth)
+{
+	std::vector<FileStruct> FnF;
+
+	if (srcPath == "") srcPath = std::filesystem::current_path().string();
+
+	_RecursiveFileSearch(FnF, srcPath, 0, depth+1);
+
+	return FnF;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFNFNameFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	for (FileStruct file : file_vec) {
+		std::string en = file._entry.path().filename().string();
+		for (std::string filter : Filters) {
+			if (en.find(filter) != std::string::npos) {
+				ret_vec.push_back(file);
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFnFCategoryFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	// Get Filters from Category
+	std::vector<std::string> all_ext_filters;
+
+	for (std::string cat : Filters) {
+		auto itb = this->_config.BaseCategories.find(cat);
+		if (itb != this->_config.BaseCategories.end())
+			all_ext_filters.insert(all_ext_filters.end(), itb->second.begin(), itb->second.end());
+
+		auto ite = this->_config.ExtendedCategories.find(cat);
+		if (ite != this->_config.ExtendedCategories.end())
+			all_ext_filters.insert(all_ext_filters.end(), ite->second.begin(), ite->second.end());
+	}
+
+	// Remove Extension Duplicates
+	std::sort(all_ext_filters.begin(), all_ext_filters.end());
+	all_ext_filters.erase(std::unique(all_ext_filters.begin(), all_ext_filters.end()), all_ext_filters.end());
+
+
+	for (FileStruct file : file_vec) {
+		std::string ext = file._entry.path().filename().extension().string();
+
+		for (std::string catfilter : all_ext_filters) {
+			if ("." + catfilter == ext) {
+				ret_vec.push_back(file);
+				break;
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFnFExtensionFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	for (FileStruct file : file_vec) {
+		std::string ext = file._entry.path().filename().extension().string();
+		for (std::string extfilter : Filters) {
+			if ("." + extfilter == ext) {
+				ret_vec.push_back(file);
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+void FileHandler::CreateFiles(std::vector<std::string> files)
+{
+	for (std::string file : files) {
+		std::ofstream c_file;
+		c_file.open(file);
+		c_file.close();
+	}
+}
+
+void FileHandler::CreateFolders(std::vector<std::string> folders)
+{
+	for (std::string folder : folders) {
+		std::filesystem::create_directory(folder);
+	}
+}
+
+void FileHandler::ShowFilesAndFolders(std::vector<FileStruct> FnF)
+{
+	std::string t_lbl;
+
+	for (FileStruct file : FnF) {
+		std::filesystem::directory_entry entry = file._entry;
+
+		if (entry.is_directory()) t_lbl = "[Directory]";
+		else if (entry.is_block_file()) t_lbl = "[Block File]";
+		else if (entry.is_character_file()) t_lbl = "[Character File]";
+		else if (entry.is_regular_file()) t_lbl = "[File]";
+
+		std::cout << std::string(file._depth, '-') << t_lbl << " -> " << "[" << file._depth << "] " << entry.path().filename().string() << "\n";
+	}
+}
+
+void FileHandler::CloneFiles(std::vector<FileStruct> FnF, std::string destPath)
+{
+	std::cout << "Starting Creating Clones For:" << '\n';
+	for (FileStruct file : FnF) {
+		std::filesystem::path dir(destPath);
+		std::filesystem::path c_file(file._entry.path().filename().string());
+		std::filesystem::path full_path = dir / c_file;
+
+		if (file._entry.is_directory()) continue;
+
+		try {
+			std::filesystem::copy_file(file._entry.path(), full_path);
+			std::cout
+				<< file._entry.path().filename().string()
+				<< " at depth of "
+				<< file._depth
+				<< " is copied successfully"
+				<< '\n';
+		}
+		catch (std::filesystem::filesystem_error& err) {
+			std::cout
+				<< "Unable to copy " << file._entry.path().filename().string() << " -> "
+				<< err.what()
+				<< '\n';
+		}
+	}
+}
+
+void FileHandler::RemoveFiles(std::vector<std::string> files)
+{
+	for (std::string file : files) {
+		if (std::filesystem::remove(file)) std::cout << "File -> " << file << " successfully deleted" << std::endl;
+		if (std::filesystem::remove(file)) std::cout << "Unable to delete" << file << std::endl;
+	}
+}
+
+void FileHandler::RemoveFolders(std::vector<std::string> folders)
+{
+	for (std::string folder : folders) {
+		if (std::filesystem::remove_all(folder)) std::cout << "Directory -> " << folder << " successfully deleted" << std::endl;
+		if (std::filesystem::remove_all(folder)) std::cout << "Unable to delete" << folder << std::endl;
+	}
+}
+
+void FileHandler::_RecursiveFileSearch(std::vector<FileStruct>& path_list, std::string dir_path, int curr_depth, int max_depth)
+{
+	if (curr_depth >= max_depth && max_depth != -1) return;
+
+	for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
+		if (entry.is_directory()) {
+			path_list.push_back(FileStruct(entry, curr_depth));
+			_RecursiveFileSearch(path_list, entry.path().string(), curr_depth+1, max_depth);
+		}
+		else {
+			path_list.push_back(FileStruct(entry, curr_depth));
+		}
+	}
+=======
+#include "FileHandler.h"
+
+void FileHandler::PrintHelp()
+{
+	std::cout
+		<< "    Create New File:                    tsft +fl <name>							\n"
+		<< "    Create New Files:                   tsft +fl \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Create New Folder:                  tsft +fd <name>							\n"
+		<< "    Create New Folders:                 tsft +fd \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Show Stuffs:                        tsft -p <depth:opt>		                \n"
+		<< "        Show All with Filter (included optional depth after -p)								\n"
+		<< "\n"
+		<< "        Substring Filters:              tsft -p <depth:opt> -sf <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -sf \"<name_1>, <name_2>, ...\"	\n"
+		<< "        Category Filters:               tsft -p <depth:opt> -cf <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -cf \"<name_1>, <name_2>, ...\"	\n"
+		<< "        Extension Filters:              tsft -p <depth:opt> -ef <name_1>					\n"
+		<< "                                        tsft -p <depth:opt> -ef \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Clone Path:                         tsft -c <depth:opt> <dest> <src:opt>				\n"
+		<< "        without passing depth explicitly, depth will be set to 0							\n"
+		<< "\n"
+		<< "        Clone with Substring													            \n"
+		<< "        Substring Filters:              tsft -c <depth> -sf \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "        Category Filters:               tsft -c <depth> -cf \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "        Extension Filters:              tsft -c <depth> -ef \"<name(s), ...>\" <dest> <src:opt>	\n"
+		<< "\n\n"
+		<< "    Remove Existing File:               tsft -fl <name>							\n"
+		<< "    Remove Existing Files:              tsft -fl \"<name_1>, <name_2>, ...\"	\n"
+		<< "\n\n"
+		<< "    Remove Existing Folder:             tsft -fd <name>							\n"
+		<< "    Remove Existing Folders:            tsft -fd \"<name_1>, <name_2>, ...\"	\n"
+		<< std::endl;
+}
+
+FileHandler::FileHandler(Config config)
+{
+	this->_config = config;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFile(std::string srcPath, int depth)
+{
+	std::vector<FileStruct> FnF;
+
+	if (srcPath == "") srcPath = std::filesystem::current_path().string();
+
+	_RecursiveFileSearch(FnF, srcPath, 0, depth+1);
+
+	return FnF;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFNFNameFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	for (FileStruct file : file_vec) {
+		std::string en = file._entry.path().filename().string();
+		for (std::string filter : Filters) {
+			if (en.find(filter) != std::string::npos) {
+				ret_vec.push_back(file);
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFnFCategoryFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	// Get Filters from Category
+	std::vector<std::string> all_ext_filters;
+
+	for (std::string cat : Filters) {
+		auto itb = this->_config.BaseCategories.find(cat);
+		if (itb != this->_config.BaseCategories.end())
+			all_ext_filters.insert(all_ext_filters.end(), itb->second.begin(), itb->second.end());
+
+		auto ite = this->_config.ExtendedCategories.find(cat);
+		if (ite != this->_config.ExtendedCategories.end())
+			all_ext_filters.insert(all_ext_filters.end(), ite->second.begin(), ite->second.end());
+	}
+
+	// Remove Extension Duplicates
+	std::sort(all_ext_filters.begin(), all_ext_filters.end());
+	all_ext_filters.erase(std::unique(all_ext_filters.begin(), all_ext_filters.end()), all_ext_filters.end());
+
+
+	for (FileStruct file : file_vec) {
+		std::string ext = file._entry.path().filename().extension().string();
+
+		for (std::string catfilter : all_ext_filters) {
+			if ("." + catfilter == ext) {
+				ret_vec.push_back(file);
+				break;
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+std::vector<FileStruct> FileHandler::GetAllFnFExtensionFiltered(std::vector<std::string> Filters, std::string srcPath, int depth)
+{
+	std::vector<FileStruct> file_vec = GetAllFile(srcPath, depth);
+	std::vector<FileStruct> ret_vec;
+
+	for (FileStruct file : file_vec) {
+		std::string ext = file._entry.path().filename().extension().string();
+		for (std::string extfilter : Filters) {
+			if ("." + extfilter == ext) {
+				ret_vec.push_back(file);
+			}
+		}
+	}
+
+	return ret_vec;
+}
+
+void FileHandler::CreateFiles(std::vector<std::string> files)
+{
+	for (std::string file : files) {
+		std::ofstream c_file;
+		c_file.open(file);
+		c_file.close();
+	}
+}
+
+void FileHandler::CreateFolders(std::vector<std::string> folders)
+{
+	for (std::string folder : folders) {
+		std::filesystem::create_directory(folder);
+	}
+}
+
+void FileHandler::ShowFilesAndFolders(std::vector<FileStruct> FnF)
+{
+	std::string t_lbl;
+
+	for (FileStruct file : FnF) {
+		std::filesystem::directory_entry entry = file._entry;
+
+		if (entry.is_directory()) t_lbl = "[Directory]";
+		else if (entry.is_block_file()) t_lbl = "[Block File]";
+		else if (entry.is_character_file()) t_lbl = "[Character File]";
+		else if (entry.is_regular_file()) t_lbl = "[File]";
+
+		std::cout << std::string(file._depth, '-') << t_lbl << " -> " << "[" << file._depth << "] " << entry.path().filename().string() << "\n";
+	}
+}
+
+void FileHandler::CloneFiles(std::vector<FileStruct> FnF, std::string destPath)
+{
+	std::cout << "Starting Creating Clones For:" << '\n';
+	for (FileStruct file : FnF) {
+		std::filesystem::path dir(destPath);
+		std::filesystem::path c_file(file._entry.path().filename().string());
+		std::filesystem::path full_path = dir / c_file;
+
+		if (file._entry.is_directory()) continue;
+
+		try {
+			std::filesystem::copy_file(file._entry.path(), full_path);
+			std::cout
+				<< file._entry.path().filename().string()
+				<< " at depth of "
+				<< file._depth
+				<< " is copied successfully"
+				<< '\n';
+		}
+		catch (std::filesystem::filesystem_error& err) {
+			std::cout
+				<< "Unable to copy " << file._entry.path().filename().string() << " -> "
+				<< err.what()
+				<< '\n';
+		}
+	}
+}
+
+void FileHandler::RemoveFiles(std::vector<std::string> files)
+{
+	for (std::string file : files) {
+		if (std::filesystem::remove(file)) std::cout << "File -> " << file << " successfully deleted" << std::endl;
+		if (std::filesystem::remove(file)) std::cout << "Unable to delete" << file << std::endl;
+	}
+}
+
+void FileHandler::RemoveFolders(std::vector<std::string> folders)
+{
+	for (std::string folder : folders) {
+		if (std::filesystem::remove_all(folder)) std::cout << "Directory -> " << folder << " successfully deleted" << std::endl;
+		if (std::filesystem::remove_all(folder)) std::cout << "Unable to delete" << folder << std::endl;
+	}
+}
+
+void FileHandler::_RecursiveFileSearch(std::vector<FileStruct>& path_list, std::string dir_path, int curr_depth, int max_depth)
+{
+	if (curr_depth >= max_depth && max_depth != -1) return;
+
+	for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
+		if (entry.is_directory()) {
+			path_list.push_back(FileStruct(entry, curr_depth));
+			_RecursiveFileSearch(path_list, entry.path().string(), curr_depth+1, max_depth);
+		}
+		else {
+			path_list.push_back(FileStruct(entry, curr_depth));
+		}
+	}
+>>>>>>> 6822adcdac7fb86e054535633119486a9acfa7de
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# TSFT
+<<<<<<< HEAD
+
+tsft (tsft.exe) is a command line tool to aid in the creation, deletion, and manipulation of windows files and folders
+
+## Installation
+
+Download/Clone this repository then add it to your environment paths to access it globally
+You may set up additional configuration on C:\\TunaSalmon\\tsft
+
+
+## Usage
+
+Use ``` tsft ``` to learn about its different functions.
+
+
+Some of the sample commands are:  
+``` tsft +fl file.md ``` will add a new file called file.md in the current directory  
+``` tsft -fd folder ``` will add a new folder called folder in the current directory  
+``` tsft -p ``` will print all items in the current directory at depth 0  
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[Unlicensed](https://unlicense.org)
+=======
+A command line tool to aid in the creation, deletion, and manipulation of windows files and folders
+
+****
+**Sample Commands**
+
+``tsft -p 2`` will display all files and folders at depth 2
+
+``tsft +fl sample.txt`` will create a sample.txt file in the current directory
+
+``tsft -fl "sample1.txt, sample2.txt"`` will delete the following files in the current directory
+>>>>>>> 48b6b6cd8ed3809c9ad1eb84316291b8a06b1db2
